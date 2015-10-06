ba87c16b4b5674bc87aa259a946433950b4cbb7e
Frank D Evans <frankdevans@yahoo.com>
2015-10-06 12:29:44 -0500

    add string rep of board, refine shots method

--- client/frank/battleship_frank.py
+++ client/frank/battleship_frank.py
@@ -13,13 +13,28 @@ class Board:
         self.ship_locations = []
         self.my_board = {}
         self.opp_board = {}
+    def __str__(self):
+        rep = {
+            'hit':'H',
+            'miss':'M',
+            'shot': 'S',
+            'empty':'_'
+        }
+        collector = []
+        for y in range(self.grid_height):
+            for x in range(self.grid_width):
+                status = self.opp_board[(x,y)]
+                collector.append(rep[status])
+                collector.append(' ')
+            collector.append('\n')
+        return ''.join(collector)
     def set_grid(self, w, h):
         self.grid_width = w
         self.grid_height = h
         for x in range(self.grid_width):
             for y in range(self.grid_height):
-                my_board[(x,y)] = 'empty'
-                opp_board[(x,y)] = 'empty'
+                self.my_board[(x,y)] = 'empty'
+                self.opp_board[(x,y)] = 'empty'
         return
     def set_num_ships(self, ships):
         self.num_ships = ships
@@ -31,14 +46,90 @@ class Board:
         return
     def record_shot(self, x, y, result):
         self.opp_board[(x,y)] = result
-    def can_ship_fit(self, x, y, pos, size):
+    def can_opp_ship_fit(self, x, y, pos, size):
         pass
     def place_ships(self):
-        pass
+        # Hardcode 5 ships
+        self.ship_locations.append((0,2,'H'))  # 2 Destroyer
+        self.ship_locations.append((0,6,'V'))  # 3 Submarine
+        self.ship_locations.append((7,0,'V'))  # 3 Battleship
+        self.ship_locations.append((6,5,'H'))  # 4 Cruiser
+        self.ship_locations.append((4,9,'H'))  # 5 Carrier
     def get_ship_locations(self):
         return self.ship_locations
+    def get_surrounding_cells(self, x, y):
+        collector = []
+        if (x + 1, y) in self.opp_board:
+            collector.append((x + 1, y))
+        if (x - 1, y) in self.opp_board:
+            collector.append((x - 1, y))
+        if (x, y + 1) in self.opp_board:
+            collector.append((x, y + 1))
+        if (x, y - 1) in self.opp_board:
+            collector.append((x, y - 1))
+        return collector
+    def get_diagonal_cells(self, x, y):
+        collector = []
+        if (x + 1, y + 1) in self.opp_board:
+            collector.append((x + 1, y + 1))
+        if (x + 1, y - 1) in self.opp_board:
+            collector.append((x + 1, y - 1))
+        if (x - 1, y + 1) in self.opp_board:
+            collector.append((x - 1, y + 1))
+        if (x - 1, y - 1) in self.opp_board:
+            collector.append((x - 1, y - 1))
+        return collector
     def get_shots(self, num_shots):
-        pass # array of tuples
+        collector = []
+        # Get orphan hits
+        for i in self.opp_board:
+            if self.opp_board[i] == 'hit':
+                surr = self.get_surrounding_cells(x = i[0], y = i[1])
+                for c in surr:
+                    if self.opp_board[c] == 'empty':
+                        collector.append(c)
+
+        #TODO: Expectorization Max
+
+        # Normal Search Grid
+        for i in self.opp_board:
+            if self.opp_board[i] in ('miss','shot'):
+                diag = self.get_diagonal_cells(x = i[0], y = i[1])
+                for c in diag:
+                    if self.opp_board[c] == 'empty':
+                        collector.append(c)
+                        self.record_shot(c[0], c[1], 'shot')
+
+
+        # remove shots with miss on all 4 sides
+        delete_shots = []
+        for i in collector:
+            surr = self.get_surrounding_cells(x = i[0], y = i[1])
+            misses = 0
+            for c in surr:
+                if self.opp_board[c] in ('miss','shot'):
+                    misses += 1
+            if misses == 4:
+                delete_shots.append(i)
+                self.record_shot(i[0], i[1], 'empty')
+
+        for i in delete_shots:
+            collector.remove(i)
+
+        # Check for reserved shots
+        for i in self.opp_board:
+            if (self.opp_board[i] == 'shot') and (i not in collector):
+                collector.append(i)
+
+        # Default Starter
+        if len(collector) == 0:
+            collector.append((5,5))
+            collector.append((4,4))
+            collector.append((3,3))
+            collector.append((6,6))
+            collector.append((7,7))
+
+        return collector[:num_shots]
 
 
 
@@ -98,7 +189,7 @@ def parse_query(board, message):
 def game_loop():
     board = Board()
     while True:
-        line = raw_input().strip('\n')
+        line = raw_input()
         assert(line.endswith("|END|"))
         if line == "|INFO|end game|END|":
             return
@@ -120,7 +211,104 @@ def game_loop():
 
 #-------------------------------------------------------------
 #Unit Tests
+
+
+'''
+board = Board()
+board.set_grid(10,10)
+print board
+for i in board.get_shots(5):
+    board.record_shot(i[0], i[1], 'miss')
+print board
+shots = board.get_shots(4)
+for i in shots[1:]:
+    board.record_shot(i[0], i[1], 'miss')
+board.record_shot(shots[0][0], shots[0][1], 'hit')
+print board
+for i in board.get_shots(5):
+    board.record_shot(i[0], i[1], 'miss')
+print board
+for i in board.get_shots(5):
+    board.record_shot(i[0], i[1], 'miss')
+print board
+for i in board.get_shots(5):
+    board.record_shot(i[0], i[1], 'miss')
+print board
+for i in board.get_shots(5):
+    board.record_shot(i[0], i[1], 'miss')
+print board
+for i in board.get_shots(5):
+    board.record_shot(i[0], i[1], 'miss')
+print board
+for i in board.get_shots(5):
+    board.record_shot(i[0], i[1], 'miss')
+print board
+
+
+board = Board()
+board.set_grid(10,10)
+print board
+shots = board.get_shots(5)
+print shots
+for i in shots:
+    board.record_shot(i[0], i[1], 'miss')
+print board
+shots = board.get_shots(4)
+for i in shots:
+    board.record_shot(i[0], i[1], 'miss')
+print board
+shots = board.get_shots(4)
+for i in shots:
+    board.record_shot(i[0], i[1], 'miss')
+print board
+
+
+board = Board()
+board.set_grid(10,10)
+print board
+print board.get_shots(5)
+board.record_shot(4,4,'miss')
+board.record_shot(5,5,'miss')
+board.record_shot(3,3,'hit')
+board.record_shot(6,6,'hit')
+board.record_shot(7,7,'miss')
+print board
+shots = board.get_shots(4)
+print shots
+for i in shots:
+    board.record_shot(i[0], i[1], 'miss')
+print board
+shots = board.get_shots(4)
+for i in shots:
+    board.record_shot(i[0], i[1], 'miss')
+print board
+
+
+board = Board()
+board.set_grid(10,10)
+print board.get_shots(5)
+print board
+board.record_shot(4,4,'miss')
+board.record_shot(5,5,'miss')
+board.record_shot(3,3,'hit')
+board.record_shot(6,6,'hit')
+board.record_shot(7,7,'miss')
+print board.opp_board
+print board.get_shots(4)
+
+
+board = Board()
+board.set_grid(10,10)
+print board.opp_board
+
+
 board = Board()
 print board.grid_width
 board.set_grid(3,4)
 print board.grid_width
+
+
+board = Board()
+board.place_ships()
+print board.get_ship_locations()
+'''
